c8b1ce015b52d6670b26e58b8818d33b16b24b9e
Prasad Vennam <vennamprasad@gmail.com>
2020-02-21 01:10:53 +0530

    design

--- app/src/main/java/com/cartravels_new/screens/activities/RegistrationTypeActivity.kt
+++ app/src/main/java/com/cartravels_new/screens/activities/RegistrationTypeActivity.kt
@@ -1,13 +1,21 @@
 package com.cartravels_new.screens.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cartravels_new.R
+import kotlinx.android.synthetic.main.activity_registration_type.*
 
 class RegistrationTypeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration_type)
+        normal.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+        business.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
     }
 }
