381056737cea49b2ded820767015b31c954c7dab
mahendhar <mahendharjakkula96@gmail.com>
2020-02-26 21:59:16 +0530

    Merge branch 'master' of F:\ThemesDemp with conflicts.

--- app/src/main/java/com/cartravels_new/screens/activities/RegistrationActivity.kt
+++ app/src/main/java/com/cartravels_new/screens/activities/RegistrationActivity.kt
@@ -1,13 +1,20 @@
 package com.cartravels_new.screens.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.cartravels_new.R
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        next.setOnClickListener {
+            val intent = Intent(RegistrationActivity@ this, ImageCaptureActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
